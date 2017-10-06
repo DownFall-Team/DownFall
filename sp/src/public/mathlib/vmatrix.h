fdbcf08d6faa62aec849848c91ab0251ce2ffc10
SCell555 <kubci.rusnk645@gmail.com>
2017-10-06 12:15:40 +0200

    Update FGD Fix vbsp

--- sp/src/public/mathlib/vmatrix.h
+++ sp/src/public/mathlib/vmatrix.h
@@ -272,7 +272,7 @@ VMatrix		SetupMatrixOrgAngles(const Vector &origin, const QAngle &vAngles);
 
 #endif
 
-#define VMatToString(mat)	(static_cast<const char *>(CFmtStr("[ (%f, %f, %f), (%f, %f, %f), (%f, %f, %f), (%f, %f, %f) ]", mat.m[0][0], mat.m[0][1], mat.m[0][2], mat.m[0][3], mat.m[1][0], mat.m[1][1], mat.m[1][2], mat.m[1][3], mat.m[2][0], mat.m[2][1], mat.m[2][2], mat.m[2][3], mat.m[3][0], mat.m[3][1], mat.m[3][2], mat.m[3][3] ))) // ** Note: this generates a temporary, don't hold reference!
+#define VMatToString(mat)	(static_cast<const char *>(CFmtStr("[ (%f, %f, %f, %f), (%f, %f, %f, %f), (%f, %f, %f, %f), (%f, %f, %f, %f) ]", mat.m[0][0], mat.m[0][1], mat.m[0][2], mat.m[0][3], mat.m[1][0], mat.m[1][1], mat.m[1][2], mat.m[1][3], mat.m[2][0], mat.m[2][1], mat.m[2][2], mat.m[2][3], mat.m[3][0], mat.m[3][1], mat.m[3][2], mat.m[3][3] ))) // ** Note: this generates a temporary, don't hold reference!
 
 //-----------------------------------------------------------------------------
 // Returns the point at the intersection on the 3 planes.
